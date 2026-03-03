47e5515f4a9dc9de2620df5377d1456aac44897e
pog7x <poluningm@gmail.com>
2026-03-03 21:49:41 +0300

    readme

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8080',
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8080',
     },
   },
 
